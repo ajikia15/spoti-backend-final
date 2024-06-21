e70964f22102f8363fd30afa7f94df8e4dd9736f
ajikia15 <aleksandrej15@gmail.com>
2024-06-21 23:27:08 +0400

    converted to jquery

--- front/script.js
+++ front/script.js
@@ -1,131 +1,123 @@
-async function fetchAlbumsByArtist(albums) {
-  const albumsList = document.getElementById("albumsList");
-  albumsList.innerHTML = "";
-  albums.forEach((album) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = album.name;
-    listItem.classList.add("cursor-pointer");
-    listItem.addEventListener("click", () => {
-      fetchSongsByAlbum(album._id);
+$(document).ready(function () {
+  async function fetchAlbumsByArtist(albums) {
+    const $albumsList = $("#albumsList");
+    $albumsList.empty();
+    albums.forEach((album) => {
+      const $listItem = $("<li>").text(album.name).addClass("cursor-pointer");
+      $listItem.on("click", () => {
+        fetchSongsByAlbum(album._id);
+      });
+      $albumsList.append($listItem);
     });
-    albumsList.appendChild(listItem);
-  });
-}
-
-async function fetchSongsByAlbum(albumId) {
-  try {
-    const response = await fetch(`http://localhost:3000/albums/${albumId}`);
-    const album = await response.json();
-    const songs = album.songs;
-    const songsList = document.getElementById("songsList");
-    songsList.innerHTML = "";
-    songs.forEach((song) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = song.name;
-      listItem.classList.add("cursor-pointer");
-      songsList.appendChild(listItem);
-    });
-  } catch (error) {
-    console.error("Error fetching songs:", error);
   }
-}
 
-async function fetchAllArtists() {
-  try {
-    const response = await fetch("http://localhost:3000/artists/all");
-    const artists = await response.json();
-    const artistsList = document.getElementById("artistList");
-    artistsList.innerHTML = "";
-    artists.forEach((artist) => {
-      const listItem = document.createElement("li");
-      listItem.textContent = artist.name;
-      listItem.classList.add("cursor-pointer");
-      listItem.addEventListener("click", () =>
-        fetchAlbumsByArtist(artist.albums)
-      );
-      artistsList.appendChild(listItem);
-    });
-  } catch (error) {
-    console.error("Error fetching artists:", error);
+  async function fetchSongsByAlbum(albumId) {
+    try {
+      const response = await fetch(`http://localhost:3000/albums/${albumId}`);
+      const album = await response.json();
+      console.log(response);
+      const songs = album.songs;
+      const $songsList = $("#songsList");
+      $songsList.empty();
+      songs.forEach((song) => {
+        const $listItem = $("<li>").text(song.name).addClass("cursor-pointer");
+        $songsList.append($listItem);
+      });
+      displaySongs(songs);
+    } catch (error) {
+      console.error("Error fetching songs:", error);
+    }
   }
-}
 
-fetchAllArtists();
+  async function fetchAllArtists() {
+    try {
+      const response = await fetch("http://localhost:3000/artists/all");
+      const artists = await response.json();
+      const $artistsList = $("#artistList");
+      $artistsList.empty();
+      artists.forEach((artist) => {
+        const $listItem = $("<li>")
+          .text(artist.name)
+          .addClass("cursor-pointer");
+        $listItem.on("click", () => fetchAlbumsByArtist(artist.albums));
+        $artistsList.append($listItem);
+      });
+    } catch (error) {
+      console.error("Error fetching artists:", error);
+    }
+  }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("loginForm");
-  const loginBtn = document.getElementById("loginBtn");
-  const registerBtn = document.getElementById("registerBtn");
+  fetchAllArtists();
 
-  form.addEventListener("submit", function (event) {
+  $("#loginForm").submit(function (event) {
     event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const username = $("#username").val();
+    const password = $("#password").val();
     loginUser(username, password);
   });
 
-  registerBtn.addEventListener("click", function (event) {
+  $("#registerBtn").click(function (event) {
     event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const username = $("#username").val();
+    const password = $("#password").val();
     registerUser(username, password);
   });
+
+  async function loginUser(username, password) {
+    const url = "http://localhost:3000/users/login";
+    const data = {
+      username: username,
+      password: password,
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const jsonResponse = await response.json();
+      console.log("Login successful:", jsonResponse);
+      localStorage.setItem("token", jsonResponse.token);
+      localStorage.setItem("username", username);
+      location.reload();
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  }
+
+  async function registerUser(username, password) {
+    const url = "http://localhost:3000/users/register";
+    const data = {
+      username: username,
+      password: password,
+      role: "user",
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const jsonResponse = await response.json();
+      console.log("Registration successful:", jsonResponse);
+    } catch (error) {
+      console.error("Error during registration:", error);
+    }
+  }
 });
-
-async function loginUser(username, password) {
-  const url = "http://localhost:3000/users/login";
-  const data = {
-    username: username,
-    password: password,
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const jsonResponse = await response.json();
-    console.log("Login successful:", jsonResponse);
-    localStorage.setItem("token", jsonResponse.token);
-    localStorage.setItem("username", username);
-    location.reload();
-  } catch (error) {
-    console.error("Error during login:", error);
-  }
-}
-
-async function registerUser(username, password) {
-  const url = "http://localhost:3000/users/register";
-  const data = {
-    username: username,
-    password: password,
-    role: "user",
-  };
-
-  try {
-    const response = await fetch(url, {
-      method: "POST", // or 'PUT'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const jsonResponse = await response.json();
-    console.log("Registration successful:", jsonResponse);
-  } catch (error) {
-    console.error("Error during registration:", error);
-  }
-}
